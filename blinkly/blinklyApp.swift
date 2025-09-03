374eb9f88a08cc3dac852ed707b2f4ad65a1e6b0
Taha <noirrsw@gmail.com>
2025-09-03 23:11:00 +0300

    Initial Commit

---
+++ blinkly/blinklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  blinklyApp.swift
+//  blinkly
+//
+//  Created by tahakacmaz on 3.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct blinklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blinklyTests/blinklyTests.swift
@@ -0,0 +1,17 @@
+//
+//  blinklyTests.swift
+//  blinklyTests
+//
+//  Created by tahakacmaz on 3.09.2025.
+//
+
+import Testing
+@testable import blinkly
+
+struct blinklyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
